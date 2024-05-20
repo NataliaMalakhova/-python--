5aeb8cf3530b0aaaa11315e3497c8c0a2d07896a
NataliaMalakhova <122223240+NataliaMalakhova@users.noreply.github.com>
2024-05-20 18:57:11 +0300

    Add logger

--- 1.Import.Module.Package/main.py
+++ 1.Import.Module.Package/main.py
@@ -1,18 +1,21 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
 from datetime import datetime
+from logger import logger
+
+path_current_date = 'current_date.log'
 
 
-def print_current_date():
+@logger(path_current_date)
+def get_current_date():
     """
     Функция для вывода текущей даты.
     """
     current_date = datetime.now().strftime("%Y.%m.%d")
-    print(f"Текущая дата: {current_date}")
+    return current_date
 
 
 if __name__ == "__main__":
-    num_employees = 10  # Генерация списка из 10 сотрудников
     employees = get_employees()  # Предполагается, что get_employees вернет список сотрудников
 
     # Вывод оригинального списка сотрудников
@@ -27,7 +30,7 @@ if __name__ == "__main__":
     # Получение данных о сотруднике с пересчитанной зарплатой
     updated_employee = calculate_salary(employees, employee_id, coefficient)
 
-    print_current_date()  # Вывод текущей даты перед расчетом зарплаты
+    print(f"Текущая дата: {get_current_date()}")  # Вывод текущей даты перед расчетом зарплаты
 
     if updated_employee:
         print("\nДанные о сотруднике с пересчитанной зарплатой:")
